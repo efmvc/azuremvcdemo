25e35e57d6857f733e4d5a441a78437838b5d722
Eric Fontaine <ericcfontaine@outlook.com>
2016-01-10 16:16:03 -0800

    Added Document DB Manual Integration

---
+++ WebApp-B2C-DotNet/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using WebApp_OpenIDConnect_DotNet_B2C.Models;
+
+namespace WebApp_OpenIDConnect_DotNet_B2C.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            var users = DocumentDBRepository<Users>.GetUsers(d => !d.Disabled);
+            return View(users);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,IdentityProvider,DisplayName,GivenName,Surname,City,Country,Email,LastLogin")] Users user)
+        {
+           // if (ModelState.IsValid)
+           // {
+                await DocumentDBRepository<Users>.CreateItemAsync(user);
+                return RedirectToAction("Index");
+          //  }
+           // return View(user);
+        }
+
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp-B2C-DotNet/Models/Users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WebApp_OpenIDConnect_DotNet_B2C.Models
+{
+    public class Users
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "identityProvider")]
+        public string IdentityProvider { get; set; }
+
+        [JsonProperty(PropertyName = "displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty(PropertyName = "givenName")]
+        public string GivenName { get; set; }
+
+        [JsonProperty(PropertyName = "surname")]
+        public string Surname { get; set; }
+
+        [JsonProperty(PropertyName = "city")]
+        public string City { get; set; }
+
+        [JsonProperty(PropertyName = "country")]
+        public string Country { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "lastLogin")]
+        public string LastLogin { get; set; }
+
+        [JsonProperty(PropertyName = "disabled")]
+        public bool Disabled { get; set; }
+    }
+}
